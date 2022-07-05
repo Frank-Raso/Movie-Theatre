0c7c5946349a46ad2846586bd870c51efb46b2f7
Frank Raso <frankraso91@gmail.com>
2022-07-05 14:04:35 -0400

    App.js,add routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,27 @@
-import {HashRouter as Router, Route} from 'react-router-dom';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import MovieList from '../MovieList/MovieList'
+import Details from '../../Details/Details';
+import AddMovie from '../AddMovie/AddMovie'
 
 function App() {
   return (
     <div className="App">
-      <h1>The Movies Saga!</h1>
-      <Router>        
+      <Router>
         <Route path="/" exact>
           <MovieList />
         </Route>
-        
+
         {/* Details page */}
+        <Route path="/details/:id">
+          <Details />
+        </Route>
 
         {/* Add Movie page */}
+        <Route path="/AddMovie">
+          <AddMovie />
+        </Route>
+
       </Router>
     </div>
   );
