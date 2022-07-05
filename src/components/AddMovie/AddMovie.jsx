c2f2248d2cbe5a49680cbab1bcbbfb9bc71833c9
Frank Raso <frankraso91@gmail.com>
2022-07-05 14:07:46 -0400

    AddMovie page

---
+++ src/components/AddMovie/AddMovie.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import { Button } from '@material-ui/core';
+
+function MovieList() {
+    const history = useHistory();
+    const dispatch = useDispatch();
+    const movies = useSelector(store => store.movies);
+
+    const back = () => {
+        console.log('BackButton')
+        history.push('/');
+    }
+    return (
+        <div>
+            <h1>AddMovie Page:</h1>
+            <Button variant="contained" color="primary" onClick={back}>Back To Movies</Button>
+
+        </div>
+    );
+}
+
+export default MovieList;
