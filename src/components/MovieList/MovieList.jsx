80ac5d7ffb521f0d6479a48694867fa7a97e5b1c
Frank Raso <frankraso91@gmail.com>
2022-07-05 14:06:48 -0400

    MovieList

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './MovieList.css'
+import { useHistory } from 'react-router-dom';
+import { Button } from '@material-ui/core';
 
 function MovieList() {
-
+    const history = useHistory();
     const dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
 
@@ -11,19 +13,32 @@ function MovieList() {
         dispatch({ type: 'FETCH_MOVIES' });
     }, []);
 
+    const details = (id) => {
+        history.push(`/details/${id}`)
+    }
+    const toAdd = () => {
+        history.push(`/AddMovie`)
+    }
+
     return (
         <main>
-            <h1>MovieList</h1>
+            <h1>Franks Theatre!</h1>
+            {/* <Button  variant="contained" color="primary" onClick={toAdd}>Add Movie Here</Button> */}
+            <h2>Now Showing :</h2>
             <section className="movies">
+
                 {movies.map(movie => {
                     return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/>
+                        <div  key={movie.id} >
+                            <div className='catalog'>
+                                <h3 className='catalogTitle' >{movie.title}</h3>
+                                <img onClick={() => details(movie.id)} src={movie.poster} alt={movie.title}  />
+                            </div>
                         </div>
                     );
                 })}
             </section>
+
         </main>
 
     );
